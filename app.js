58a9e8bf6cf36be5c482034e3297d035542576cd
Ray-Shakex <bshakex@gmail.com>
2022-01-19 21:50:15 +0200

    toggle.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("navbar");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+
+    const links = document.querySelectorAll(".nav-link");
+    links.forEach( link => {
+        link.addEventListener("click", () => {
+            nav.classList.remove("active");
+        })
+    })
+})
