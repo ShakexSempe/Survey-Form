352c182839364246bd11589d5d4646afc5354014
Ray-Shakex <bshakex@gmail.com>
2022-01-20 20:28:47 +0200

    toggle active class states

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const nav = document.getElementById("navbar");
 
 toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
-    toggle.classList.toggle("active")
+    toggle.classList.toggle("active");
     const links = document.querySelectorAll(".nav-link");
     links.forEach( link => {
         link.addEventListener("click", () => {
             nav.classList.remove("active");
+            toggle.classList.remove("active");
+
         });
     });
     nav.addEventListener("click", () => {
         nav.classList.remove("active");
+        toggle.classList.remove("active");
     })
 })
